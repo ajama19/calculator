75000a5376be3234a07b72cc2ffe159d79888533
Amal Jama <amal_839@hotmail.com>
2022-12-13 16:48:21 -0500

    added starting files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Background Information:
+
+> This is a calculator created to test how well I have grasped the fundamentals of JavaScript.
+
+# Live Preview: 
+
+> coming soon!
+
+# Languages Used:
+
+> [![My Skills](https://skillicons.dev/icons?i=js,html,css)](https://skillicons.dev)
+
---
+++ script.js
@@ -0,0 +1,46 @@
+/* script.js */
+
+
+
+//function for calculator operation
+function operate() {
+    return;
+}
+
+
+//math functions
+function add() {
+    return;
+}
+
+function subtract() {
+    return;
+}
+
+
+function multiply() {
+    return;
+}
+
+
+function divide() {
+    return;
+}
+
+
+
+/* EXTRA FUNCTIONS TO ADD LATER
+
+function exponent() {
+    return;
+}
+
+
+function squareRoot() {
+    return;
+}
+
+function percent() {
+    return;
+}
+*/
