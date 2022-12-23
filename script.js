47f3f97be2e5246d3937dd7e7973a71a4d2b0fbf
Amal Jama <amal_839@hotmail.com>
2022-12-23 01:27:06 -0500

    added dark/light mode

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
 /* script.js */
 
-//variables for display
+//function for dark/light mode
+let calcMode = 'light';
+let slider = document.querySelector(".slider");
+let buttons = document.querySelectorAll("button");
+let background = document.querySelector("body");
+let screen = document.querySelector(".screen");
+let footer = document.querySelector(".footer");
+
+slider.addEventListener('click', () => {
+    if (calcMode == 'light') {
+        calcMode = 'dark';
+        background.style.backgroundColor = '#272222';
+        screen.style.backgroundColor = '#574E4E';
+        screen.style.color = '#E7E0CF';
+        footer.style.backgroundColor = '#574E4E';
+        footer.style.color = 'white';
+        for (const item of buttons) {
+            if (item.className == 'sqrt' || item.className == 'power' || item.className == 'delete' || 
+            item.className == 'pi' || item.className == 'percent' || item.className == 'int' || 
+            item.className == 'divd' || item.className == 'mult' || item.className == 'sub' 
+            || item.className == 'add' || item.className == 'equals') {
+                item.style.backgroundColor = '#E7E0CF';
+                item.style.color = '#332D2D';
+            } else {
+                item.style.backgroundColor = '#574E4E';
+                item.style.color = '#E7E0CF';
+            }
+            
+        }
+        //console.log(calcMode);
+    } else if (calcMode == 'dark') {
+        calcMode = 'light';
+        background.style.backgroundColor = '#FFFEFC';
+        screen.style.backgroundColor = '#E7E0CF';
+        screen.style.color = '#332D2D';
+        footer.style.backgroundColor = '#E7E0CF';
+        footer.style.color = '#332D2D';
+        for (const item of buttons) {
+            if (item.className == 'sqrt' || item.className == 'power' || item.className == 'delete' || 
+            item.className == 'pi' || item.className == 'percent' || item.className == 'int' || 
+            item.className == 'divd' || item.className == 'mult' || item.className == 'sub' 
+            || item.className == 'add' || item.className == 'equals') {
+                item.style.backgroundColor = '#574E4E';
+                item.style.color = '#fffaed';
+
+            } else {
+                item.style.backgroundColor = '#E7E0CF';
+                item.style.color = '#574E4E';
+            } 
+        }
+        //console.log(calcMode);
+    }
+    
+}, false);
 
 
 
